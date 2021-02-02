62a14ac76c175d7de1a32d4892e8b946b8e0ff53
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-02 17:12:37 -0500

    listings router restricted, allows /get and /post with auth

--- api/listings/listings-router.js
+++ api/listings/listings-router.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const { validateListing } = require("../middleware/listings-inputs");
+const { verifyAuth } = require("../middleware/restricted");
 
 const Listings = require("../listings/listings-model");
 
-router.get("/", (req, res) => {
+router.get("/", verifyAuth, (req, res) => {
   Listings.getAll()
     .then((listings) => {
       res.status(200).json(listings);
@@ -16,7 +17,7 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", validateListing, (req, res) => {
+router.post("/", verifyAuth, validateListing, (req, res) => {
   const listing = req.body;
   Listings.add(listing)
     .then((listing) => {
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,19 +1,23 @@
 const jwt = require("jsonwebtoken");
 const { jwtSecret } = require("../../config/secrets");
 
-module.exports = (req, res, next) => {
+module.exports = {
+  verifyAuth,
+};
+
+function verifyAuth(req, res, next) {
   const token = req.headers.authorization;
 
   if (token) {
     jwt.verify(token, jwtSecret, (err, decoded) => {
       if (err) {
-        res.status(401).json("token invalid");
+        return res.status(401).json("token invalid");
       } else {
         req.decodedJwt = decoded;
         next();
       }
     });
   } else {
-    res.status(401).json("token required");
+    return res.status(401).json("token required");
   }
-};
+}
