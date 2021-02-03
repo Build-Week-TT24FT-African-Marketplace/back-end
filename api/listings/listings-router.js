235afab790890ddbedb777395948ee27c6061b1b
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-03 12:53:27 -0500

    validateId for listing put functioning

--- api/listings/listings-router.js
+++ api/listings/listings-router.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const { validateListing } = require("../middleware/listings-inputs");
+const {
+  validateListing,
+  validateId,
+} = require("../middleware/listings-inputs");
 const { verifyAuth } = require("../middleware/restricted");
 
 const Listings = require("../listings/listings-model");
@@ -26,6 +29,18 @@ router.post("/", verifyAuth, validateListing, (req, res) => {
     .catch((err) => {
       res.status(500).json({ error: err });
     });
+});
+
+router.put("/:id", verifyAuth, validateId, validateListing, (req, res) => {
+  const listing = req.body;
+  res.status(200).json({ message: "great job!" });
+  // Listings.edit(listing)
+  //   .then((listing) => {
+  //     res.status(201).json(listing);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).json({ error: err });
+  //   });
 });
 
 module.exports = router;
--- api/middleware/listings-inputs.js
+++ api/middleware/listings-inputs.js
@@ -1,5 +1,8 @@
+const db = require("../data/db-config");
+
 module.exports = {
   validateListing,
+  validateId,
 };
 
 async function validateListing(req, res, next) {
@@ -20,8 +23,25 @@ async function validateListing(req, res, next) {
   ) {
     res.status(401).json({
       message:
-        "missing credential(s): listing_name, listing_description, listing_price, marketplace_id and/or user_id",
+        "Missing credential(s): listing_name, listing_description, listing_price, marketplace_id and/or user_id",
     });
+  } else {
+    return next();
+  }
+}
+
+async function validateId(req, res, next) {
+  const { listing_id } = req.body;
+  if (!listing_id) {
+    return res.status(400).json({ message: "Please provide a valid ID" });
+  }
+  const listingWithId = await db("listings")
+    .where("listing_id", listing_id)
+    .first();
+  if (!listingWithId) {
+    return res
+      .status(400)
+      .json({ message: `Listing with ID ${listing_id} does not exist` });
   } else {
     return next();
   }
